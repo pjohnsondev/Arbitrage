a6c3c2b166af6996ac34d5b00ea33971d9b39b4c
pjohnsondev <33270824+pjohnsondev@users.noreply.github.com>
2020-02-17 08:57:36 +1100

    Update API

--- modules/krwAud.php
+++ modules/krwAud.php
@@ -6,7 +6,7 @@
 function krwaud(){
 	$co = file_get_contents(__DIR__."/../json/coinOne.JSON");
 	$co = json_decode($co, true);
-        $url = 'https://api.fixer.io/latest?symbols=AUD,KRW';
+        $url = 'https://api.exchangeratesapi.io/latest?base=AUD&symbols=KRW';
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_TIMEOUT, 60);
 	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
@@ -14,10 +14,10 @@ function krwaud(){
 	$data = curl_exec($ch);
 	curl_close($ch);
 	$sent = json_decode($data, true);
-	if(!$sent["rates"]["KRW"] || !$sent["rates"]["AUD"]){
+	if(!$sent["rates"]["KRW"]){
 		exit;
 	}
-	$parseRate = $sent["rates"]["KRW"]/$sent["rates"]["AUD"];
+	$parseRate = $sent["rates"]["KRW"];
 	$parseRate = floatval($parseRate);
 	$co["exchange"]["rate"] = $parseRate;
 	$co = json_encode($co);
